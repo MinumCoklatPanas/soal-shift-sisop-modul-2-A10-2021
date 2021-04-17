7abf6e1d0e5438fb1bf00db3bb0732d376475ad9
MinumCoklatPanas <akumahappanatuh@gmail.com>
2021-04-17 22:50:26 +0800

    add soal1

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -1,5 +1,4 @@
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,224 +6,213 @@
 #include <errno.h>
 #include <unistd.h>
 #include <syslog.h>
+#include <string.h>
+#include <stdbool.h>
 #include <time.h>
+#include <wait.h>
+#include <dirent.h>
 
-char *music[] = {"https://drive.google.com/uc?id=1ZG8nRBRPquhYXq_sISdsVcXx5VdEgi-J&export=download","Musik_for_Stevany.zip","Musyik","MUSIK"};
-char *film[] = {"https://drive.google.com/uc?id=1ktjGgDkL0nNpY-vT7rT7O6ZI47Ke9xcp&export=download","Film_for_Stevany.zip"," Fylm","FILM"};
-char *fotoo [] = {"https://drive.google.com/uc?id=1FsrAzb9B5ixooGUs0dGiBr-rC7TS9wTD&export=download","Foto_for_Stefany.zip","Pyoto","FOTO"};
-    
-const char * getSystemTime()
+void initDaemon()
 {
-    struct timeval tp;
-    struct tm *t;
-    char *current_time;
-    time_t curtime;    
-    
-    //gettimeofday(&tp, 0); //Pass tp to library function gettimeofday()
-    curtime = tp.tv_sec;
-    t = localtime(&curtime);
-    
-    current_time = (char *) malloc(sizeof(char) * 18);
-    sprintf(current_time,"%02d:%02d:%02d:%04ld", 
-                         t->tm_hour, t->tm_min, t->tm_sec, tp.tv_usec/1000);
-    return current_time;
+  pid_t pid, sid;        // Variabel untuk menyimpan PID
+
+  pid = fork();     // Menyimpan PID dari Child Process
+
+  /* Keluar saat fork gagal
+  * (nilai variabel pid < 0) */
+  if (pid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  /* Keluar saat fork berhasil
+  * (nilai variabel pid adalah PID dari child process) */
+  if (pid > 0) {
+    exit(EXIT_SUCCESS);
+  }
+
+  umask(0);
+
+  sid = setsid();
+  if (sid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if ((chdir("/home/pan/sisop/soal-shift-sisop-modul-2-A10-2021")) < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+  // while (1) {
+  //   // Tulis program kalian di sini
+  //   int a = system("mkdir `date`");
+  //   sleep(10);
+  // }
 }
 
-void pre_ultah(){
-    pid_t start = fork();
-    int statusstart;
-    
-    if(start<0) exit(EXIT_FAILURE);
-    
-        if(start==0){
-            pid_t awal = fork();
-            int statusawal;
-         
-            if(awal<0) exit(EXIT_FAILURE);
-         
-            if(awal==0){
-                char *argv[]={"mkdir","-p",music[2], film[2], fotoo[2], NULL};
-                execv("/usr/bin/mkdir",argv);
-            }
-            else{
-                while((wait(&statusawal)) > 0);
-                pid_t child1 = fork();
-                int status1;
-             
-                if(child1<0) exit(EXIT_FAILURE);
-             
-                if(child1==0){
-                    pid_t child2 = fork();
-                    int status2;
-                 
-                    if(child2<0) exit(EXIT_FAILURE);
-                 
-                    if(child2==0){
-                        pid_t child3 = fork();
-                        int status3;
-                     
-                        if(child3<0) exit(EXIT_FAILURE);
-                     
-                        if(child3==0){// musik
-                            pid_t child4 = fork();
-                            int status4;
-                         
-                            if(child4<0) exit(EXIT_FAILURE);
-                         
-                            if(child4==0){
-                                char *argv[] = {"wget","-q","--no-check-certificate",music[0],"-O",music[1], NULL};
-                                execv("/usr/bin/wget",argv);
-                            }
-                            else{
-                                while((wait(&status4)) > 0);
-                                char *argv[] = {"unzip", music[1], NULL};
-                                execv("/usr/bin/unzip", argv);
-                            }
-                        }
-                        else{// film
-                            while((wait(&status3)) > 0);
-                            pid_t child5 = fork();
-                            int status5;
-                         
-                            if(child5<0) exit(EXIT_FAILURE);
-                         
-                            if(child5 == 0){
-                                char *argv[] = {"wget","-q","--no-check-certificate",film[0],"-O",film[1], NULL};
-                                execv("/usr/bin/wget", argv);
-                            }
-                            else{
-                                while((wait(&status5)) > 0);
-                                char *argv[] = {"unzip", film[1], NULL};
-                                execv("/usr/bin/unzip", argv);
-                            }
-                        }
-                    }
-                    else{// foto
-                        while((wait(&status2)) > 0);
-                        pid_t child6 = fork();
-                        int status6;
-                     
-                        if(child6<0) exit(EXIT_FAILURE);
-                     
-                        if(child6 == 0){
-                            char *argv[] = {"wget","-q","--no-check-certificate",fotoo[0],"-O",fotoo[1], NULL};
-                            execv("/usr/bin/wget", argv);
-                        }
-                        else{
-                            while((wait(&status6)) > 0);
-                            char *argv[] = {"unzip", fotoo[1], NULL};
-                            execv("/usr/bin/unzip", argv);
-                        }
-                    }
-                }
-                else{
-                    while((wait(&status1)) > 0);
-                    pid_t child7 = fork();
-                    int status7;
-                     
-                    if(child7<0) exit(EXIT_FAILURE);
-                     
-                    if(child7==0){
-                        pid_t child8 = fork();
-                        int status8;
-                         
-                        if(child8<0) exit(EXIT_FAILURE);
-                         
-                        if(child8==0){//move musik
-                            char *argv[] = {"find", "/home/velli/modul2/MUSIK", "-type","f","-exec","mv","{}","/home/velli/modul2/Musyik",";", NULL};
-                            execv("/usr/bin/find", argv);
-                        }
-                        else{//move film
-                            while((wait(&status8)) > 0);
-                            char *argv[] = {"find", "/home/velli/modul2/FILM", "-type","f","-exec","mv","{}","/home/velli/modul2/Fylm",";", NULL};
-                            execv("/usr/bin/find", argv);
-                        }
-                    }
-                    else{//move foto
-                        while((wait(&status7)) > 0);
-                        char *argv[] = {"find","/home/velli/modul2/FOTO","-type","f","-exec","mv","{}","/home/velli/modul2/Pyoto",";", NULL};
-                        execv("/usr/bin/find", argv);
-                    }
-                }
-            }
-        }
-        else{
-            while((wait(&statusstart)) > 0);
-            return;
-        }
-}
-
-void ultah(){
-    pid_t lili = fork();
-    int statuslili;
-    
-    if(lili<0) exit(EXIT_FAILURE);
-    
-    if(lili==0){
-        pid_t child9 = fork();
-            int status9;
-         
-            if(child9<0) exit(EXIT_FAILURE);
-
-            if(child9==0){//ngezip
-                char *argv[]={"zip","-mr","/home/velli/modul2/Lopyu_Stevany",music[2], film[2], fotoo[2], NULL};
-                execv("/usr/bin/zip",argv);
-            }
-            else{// delete empty folder
-                while((wait(&status9)) > 0);
-                char *argv[]={"rm","-r",music[3], film[3], fotoo[3], NULL};
-                execv("/usr/bin/rm",argv);
-            }   
+void execute(char *arg[],char path[])
+{
+    pid_t pid;
+    int status;
+    pid = fork();
+    if (pid == 0)
+    {
+        execv(path,arg);
     }
-    else{
-        while((wait(&statuslili)) > 0);
+    while (wait(&status) > 0);
+    return;
+}
+
+void makeFolder(char name[])
+{
+    char* argv[] = {"mkdir",name,NULL};
+    char path[] = "/bin/mkdir";
+    execute(argv,path);
+}
+
+void unzip()
+{
+    char* argv[] = {"unzip","/home/pan/sisop/soal-shift-sisop-modul-2-A10-2021/tmp.zip",NULL};
+    char path[] = "/usr/bin/unzip";
+    execute(argv,path);
+}
+void download(char url[])
+{
+    char* argv[] = {"wget","--no-check-certificate",url,"-O","tmp.zip",NULL};
+    char path[] = "/usr/bin/wget";
+    execute(argv,path);
+    // unzip();
+    // execl("/usr/bin/wget","wget",url,NULL);
+}
+
+void downloadPics()
+{
+    time_t timer;
+    char buffer[35];
+    struct tm* tm_info;
+
+    timer = time(NULL);
+    tm_info = localtime(&timer);
+    strftime(buffer,35,"%Y-%m-%d_%H:%M:%S",tm_info);
+    // printf("%s",buffer);
+    // puts("yo");
+    // return;
+    int sz = (timer % 1000) + 50;
+    char url[] = "https://picsum.photos/";
+    char tmp[55];
+    sprintf(tmp,"%d",sz);
+    strcat(url,tmp);
+    printf("%s\n",url);
+    pid_t pid;
+    int status;
+    pid = fork();
+    if (pid == 0)
+    {
+        char *arg[] = {"wget",url,"-O",buffer,NULL};
+        execv("/usr/bin/wget",arg);
+    }
+    while (wait(&status) > 0);
+    return;
+}
+
+void moveFile(char sourcePath[],char destPath[])
+{
+    char* argv[] = {"mv",sourcePath,destPath,NULL};
+    char path[] = "/bin/mv";
+    execute(argv,path);
+}
+
+void listFilesRecursively(char *basePath,char nama[])
+{
+    char path[1010];
+    struct dirent *dp;
+    DIR *dir = opendir(basePath);
+
+    if (!dir)
         return;
+
+    while ((dp = readdir(dir)) != NULL)
+    {
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
+        {
+            // printf("%s\n", dp->d_name);
+
+
+            // Construct new path from our base path
+            strcpy(path, basePath);
+            strcat(path, "/");
+            strcat(path, dp->d_name);
+
+            char destPath[1010];
+            strcpy(destPath,"/home/pan/sisop/soal-shift-sisop-modul-2-A10-2021/");
+            strcat(destPath,nama);
+
+            moveFile(path,destPath);
+
+            listFilesRecursively(path,nama);
+        }
     }
+
+    closedir(dir);
 }
 
-int main() {
-    pid_t pid, sid;   // Variabel untuk menyimpan PID
-    pid = fork();   // Menyimpan PID dari Child Process
-    
-    /* Keluar saat fork gagal
-    * (nilai variabel pid < 0) */
-    if (pid < 0) {
-        exit(EXIT_FAILURE);
-    }
-    
-    /* Keluar saat fork berhasil
-    * (nilai variabel pid adalah PID dari child process) */
-    if (pid > 0) {
-        exit(EXIT_SUCCESS);
-    }
-    
-    umask(0);
-    
-    sid = setsid();
-    if (sid < 0) {
-        exit(EXIT_FAILURE);
-    }
-    
-    if ((chdir("/")) < 0) {
-        exit(EXIT_FAILURE);
-    }
-    
-    close(STDIN_FILENO);
-    close(STDOUT_FILENO);
-    close(STDERR_FILENO);
-    
-    while(1){
-        time_t waktu;
-        struct tm* tm_now;
 
-        waktu = time(NULL);
-        tm_now = localtime(&waktu);
-         
-        if(tm_now->tm_mon==3 && tm_now->tm_mday==9 && tm_now->tm_hour==16 && tm_now->tm_min==22 && tm_now->tm_sec==0){
-            pre_ultah();
+void deleteZip()
+{
+    char* argv[] = {"rm","tmp.zip",NULL};
+    char path[] = "/bin/rm";
+    execute(argv,path);
+}
+
+void deleteFolder(char nama[])
+{
+    char* argv[] = {"rm","-r",nama,NULL};
+    char path[] = "/bin/rm";
+    execute(argv,path);
+}
+
+void downloadAndExtract(char url[],char dirName[],char namaBawaan[])
+{
+    makeFolder(dirName);
+    download(url);
+    unzip();
+    deleteZip();
+    char initPath[1010];
+    strcpy(initPath,"/home/pan/sisop/soal-shift-sisop-modul-2-A10-2021/");
+    strcat(initPath,namaBawaan);
+    listFilesRecursively(initPath,dirName);
+    deleteFolder(namaBawaan);
+}
+
+int main()
+{
+    // initDaemon();
+    
+    while (1)
+    {
+        time_t timer;
+        char buffer[35];
+        struct tm* tm_info;
+
+        timer = time(NULL);
+        tm_info = localtime(&timer);
+        strftime(buffer,35,"%m-%d_%H:%M",tm_info);
+        printf("%s\n",buffer);
+        if (strcmp(buffer,"04-09_16:22") == 0)
+        {
+            downloadAndExtract("https://drive.google.com/uc?id=1ZG8nRBRPquhYXq_sISdsVcXx5VdEgi-J&export=download","Musyik","MUSIK");
+            downloadAndExtract("https://drive.google.com/uc?id=1FsrAzb9B5ixooGUs0dGiBr-rC7TS9wTD&export=download","Fyoto","FOTO");
+            downloadAndExtract("https://drive.google.com/uc?id=1ktjGgDkL0nNpY-vT7rT7O6ZI47Ke9xcp&export=download","Fylm","FILM");
         }
-        else if(tm_now->tm_mon==3 && tm_now->tm_mday==9 && tm_now->tm_hour==22 && tm_now->tm_min==22 && tm_now->tm_sec==0){
-            ultah();           
+        if (strcmp(buffer,"04-09_22:22") == 0)
+        {
+            char* arg[] = {"zip","-r","Lopyu_Stevany.zip","Musyik","Fyoto","Fylm",NULL};
+            char path[] = "/usr/bin/zip";
+            execute(arg,path);
         }
-    }sleep(1);
+        sleep(1000);
+        // break;
+    }
 }
